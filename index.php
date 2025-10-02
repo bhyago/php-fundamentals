df1997147d51fe3a1696cc62d54f1d52ad2e75a1
bhyago <bhyagomesquita@gmail.com>
2025-10-02 18:44:15 -0300

    feat: inclui conceitos de variaveis em PHP
    As variáveis em PHP são usadas para armazenar dados que podem ser manipulados ao longo do código. Em PHP, uma variável é declarada começando com o símbolo $ seguido de um nome intuitivo sobre o que ela armazena, como $nome ou $idade. Ao declarar uma variável, você pode atribuir valores a ela, como números, strings ou até objetos e arrays.

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Meu portifolio</title>
+</head>
+
+<body>
+  <?php 
+    $nome = "Hyago Mesquita";
+    $saudacao = "Oi";
+    $titulo = $saudacao . ", eu sou " . $nome;
+    $subtitulo = "Seja bem vindo ao meu portifolio!";
+    $ano = 2025
+  ?>
+  <h1><?=$titulo?></h1>
+
+  <p><?=$subtitulo?></p>
+  <p><?=$ano?></p>
+
+</body>
+
+</html>
